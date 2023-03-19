acfefffd4999a6cfa5fdc9d051b944d8cbfc7e39
be-prado <beprado@umich.edu>
2023-03-19 13:41:03 -0400

    integrated my plots with main function

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+import beprado_eda
+
+
+
 def main():
-    a = 1
+    
+    beprado_figs = bernardo_eda.run_eda_analysis()
+    for fig in beprado_figs:
+        fig.show()
+
     return
     
+
+main()
