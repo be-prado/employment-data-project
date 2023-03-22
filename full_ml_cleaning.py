63df769882bdde7ac01e1ed19ad5c7321a48444c
Joe Pitti <jpitti@umich.edu>
2023-03-22 17:30:39 -0400

    added my cleaning to main cleaning file

--- full_ml_cleaning.py
+++ full_ml_cleaning.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 # individual data cleaning modules
 import beprado_ml_cleaning
+import jpitti_ml_cleaning
 
 
 def run_full_ml_cleaning():
@@ -16,5 +17,8 @@ def run_full_ml_cleaning():
     print("\nRunning beprado_ml_cleaning...")
     df = beprado_ml_cleaning.run_ml_cleaning(df)
     print("Done with beprado_ml_cleaning!\n")
+    print("\nRunning jpitti_ml_cleaning...")
+    df = jpitti_ml_cleaning.run_ml_cleaning(df)
+    print("Done with jpitti_ml_cleaning!\n")
 
     return df
