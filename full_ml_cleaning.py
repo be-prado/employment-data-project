7c14ef3d0bfd371fca5eb909bdb8cb7304736d17
be-prado <beprado@umich.edu>
2023-03-22 16:42:42 -0400

    added full cleaning module as well as my own cleaning module

---
+++ full_ml_cleaning.py
@@ -0,0 +1,20 @@
+import csv
+import pandas as pd
+import numpy as np
+# individual data cleaning modules
+import beprado_ml_cleaning
+
+
+def run_full_ml_cleaning():
+    # do general data cleaning
+    df = pd.read_csv("./unprocessed_dataset.zip")
+    ml_vars = df[['YYYY', 'YYYYMM', 'INCOME', 'HOMEAMT', 'HOMEOWN', 'INVAMT', 'AGE', 'REGION', 'SEX', 'MARRY', 'EGRADE', 'EHSGRD', 'ECLGRD', 'POLAFF']]
+    df = ml_vars.replace(r'^\s*$', np.nan, regex=True)
+    df = df.dropna()
+    df = df.apply(pd.to_numeric, axis=1)
+    # do variable specific data cleaning
+    print("\nRunning beprado_ml_cleaning...")
+    df = beprado_ml_cleaning.run_ml_cleaning(df)
+    print("Done with beprado_ml_cleaning!\n")
+
+    return df
