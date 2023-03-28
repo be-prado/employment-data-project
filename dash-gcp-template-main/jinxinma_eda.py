d976243a0b64528d2c4d038407b1b3d73b5287aa
Joe Pitti <jpitti@umich.edu>
2023-03-28 18:57:40 -0400

    minor fix to eda

--- dash-gcp-template-main/jinxinma_eda.py
+++ dash-gcp-template-main/jinxinma_eda.py
@@ -1,8 +1,8 @@
-import csv
-import numpy as np
+# import csv
+# import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 import plotly.express as px
 import pandas as pd
 
